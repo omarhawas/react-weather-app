968adec149b1c31284f78dd39403df5f8b8ac445
Omar Hawas <omarhawas@Omars-MacBook-Air.local>
2022-02-21 00:02:38 -0500

    fixing token

--- src/utils/weatherApi.js
+++ src/utils/weatherApi.js
@@ -1,10 +1,10 @@
 import axios from "axios";
-const TOKEN = process.env.WEATHER_API_TOKEN;
+// const TOKEN = process.env.WEATHER_API_TOKEN;
 
 export const getGeoCode = async (city) => {
   try {
     const response = await axios.get(
-      `http://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=1&appid=${TOKEN}
+      `http://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=1&appid=${process.env.WEATHER_API_TOKEN}
       `
     );
     return response;
@@ -16,7 +16,7 @@ export const getGeoCode = async (city) => {
 export const getWeather = async (lat, lon) => {
   try {
     const response = await axios.get(
-      `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&appid=${TOKEN}
+      `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&appid=${process.env.WEATHER_API_TOKEN}
       `
     );
     return response;
